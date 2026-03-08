415b395f4f99dba7bd1e85c7f690e6e2ff8268ea
Massimiliano Casini <massimiliano.casini@gmail.com>
2026-03-08 17:38:31 +0000

    Update

--- transform/multiply10.js
+++ transform/multiply10.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 // Wrap everything in a function (no global variable pollution)
 // variable "input" contains data passed by openhab
 (function(inputData) {
@@ -6,13 +6,5 @@
     // on write: openHAB command as string
     var MULTIPLY_BY = 10;
     return Math.round(parseFloat(inputData, 10) * MULTIPLY_BY);
-=======
-// Wrap everything in a function (no global variable pollution)
-// variable "input" contains data passed by openhab
-(function(inputData) {
-    // on read: the polled number as string
-    // on write: openHAB command as string
-    var MULTIPLY_BY = 10;
-    return Math.round(parseFloat(inputData, 10) * MULTIPLY_BY);
->>>>>>> 41fbcb2dc5539d9c9933a2433377aba8de4480c6
+
 })(input)
